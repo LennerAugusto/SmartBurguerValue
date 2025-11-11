9349d4d1d7eeddc43d5dc822a103536c3d1a3081
LennerAugusto <Lenner_augusto22@hotmail.com.br>
2025-11-11 07:07:09 -0300

    feat:add-ping-controller

---
+++ src/SmartBurguerValueAPI/Controllers/PingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartBurguerValueAPI.Controllers
+{
+    public class PingController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("API está rodando!");
+        }
+    }
+}
